2bf8f61d461a5e9488d49ab3362ca68b096988a3
rbsantos-code <richardbriansantos91@yahoo.com>
2022-03-30 21:57:11 -0700

    create reducers and test

--- client/src/__test__/reducers.test.js
+++ client/src/__test__/reducers.test.js
@@ -1,3 +1,5 @@
+import { reducer } from '../utils/reducers';
+
 // import our actions
 import {
     UPDATE_PRODUCTS,
@@ -22,4 +24,26 @@ test('UPDATE_PRODUCTS', () => {
 
     expect(newState.products.length).toBe(2);
     expect(initialState.products.length).toBe(0);
+});
+
+
+test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES,
+        categories: [{}, {}]
+    });
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+});
+
+
+test('UPDATE_CURRENT_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CURRENT_CATEGORY,
+        currentCategory: '2'
+    });
+
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
 });
