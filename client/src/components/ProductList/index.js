6f8a685b3636fbc96d843c4eeea0a8b7771be4c7
rbsantos-code <richardbriansantos91@yahoo.com>
2022-04-26 14:33:53 -0700

    comment out switch breaking app

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -17,19 +17,28 @@ function ProductList() {
 
   useEffect(() => {
     // if there's data to be stored
-    if (data) {
-      // let's store it in the global state object
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products
-      });
+   if (data) {
+     dispatch({
+       type: UPDATE_PRODUCTS,
+       products: data.products
+     });
 
-      // but let's also take each product and save it to IndexDB using the helper function
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
-      });
-    }
-  }, [data, dispatch]);
+     data.products.forEach((products) => {
+       idbPromise('products', 'put', products);
+     });
+
+     // add else if to check if `loading` is undefined in `useQuery()` Hook
+   } else if (!loading) {
+     // since we're offline, get all of the data from the 'products' store
+     idbPromise('products', 'get').then((products) => {
+       // use retrieved data to set global state for offline browsing
+       dispatch({
+         type: UPDATE_PRODUCTS,
+         products: products
+       });
+     });
+   }
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -9,7 +9,7 @@ export function pluralize(name, count) {
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
     // open connection to the database `shop-shop` with the version of 1
-    const request = window.indexeDB.open('shop-shop, 1');
+    const request = window.indexedDB.open('shop-shop, 1');
 
     // create variables to hold reference to the database, transaction (tx),. and object store
     let db, tx, store;
@@ -24,7 +24,7 @@ export function idbPromise(storeName, method, object) {
     };
 
     // handle any errors with connecting
-    request.oneerror = function(e) {
+    request.onerror = function(e) {
       console.log('There was an error');
     };
 
@@ -39,28 +39,28 @@ export function idbPromise(storeName, method, object) {
       store = tx.objectStore(storeName);
 
       // if there's any errors, let us know
-      db.oneerror = function(e) {
+      db.onerror = function(e) {
         console.log('error', e);
       };
 
-      switch(method) {
-        case 'put':
-          store.put(object);
-          resolve(object);
-          break;
-        case 'get':
-          const all = store.getAll();
-          all.onsuccess = function() {
-            resolve(all.result);
-          };
-          break;
-        case 'delete':
-          store.delete(object._id);
-          break;
-        default:
-          console.log('No valid method');
-          break;
-      }
+      // switch (method) {
+      //   case 'put':
+      //     store.put(object);
+      //     resolve(object);
+      //     break;
+      //   case 'get':
+      //     const all = store.getAll();
+      //     all.onsuccess = function() {
+      //       resolve(all.result);
+      //     };
+      //     break;
+      //   case 'delete':
+      //     store.delete(object._id);
+      //     break;
+      //   default:
+      //     console.log('No valid method');
+      //     break;
+      // }
 
       // when the transaction is complete, close the connection
       tx.oncomplete = function() {
